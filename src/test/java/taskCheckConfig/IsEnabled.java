d8647da64bc9379feef844e9ce63d2bd5062c0a3
robertdobrowolski001 <robertdobrowolski001@gmail.com>
2023-06-26 18:39:48 +0200

    Class IsEnabled

--- src/test/java/taskCheckConfig/IsEnabled.java
+++ src/test/java/taskCheckConfig/IsEnabled.java
@@ -2,13 +2,20 @@ package taskCheckConfig;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GetTagName {
-    public static void main(String[] args) {
+public class IsEnabled {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://pl.wikipedia.org/");
-        String tag = driver.findElement(By.id("main-page-content")).getTagName();
-        System.out.println(tag);
+        Thread.sleep(2000); //wrzucamy sleep, aby obejsc wyjatek zwiazany ze zmianami na stronie po wywolaniu
+        WebElement search = driver.findElement(By.name("search"));
+        if(search.isEnabled()) {
+            search.sendKeys("Selenium");
+            search.submit();
+        }else {
+            System.out.println("Element is disabled");
+        }
     }
 }
