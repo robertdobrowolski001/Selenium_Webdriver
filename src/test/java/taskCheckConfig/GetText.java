30b9bb9e5c3d13910df295bbd4b9ff2cba020ca1
robertdobrowolski001 <robertdobrowolski001@gmail.com>
2023-06-26 18:37:42 +0200

    Class GetText

--- src/test/java/taskCheckConfig/GetText.java
+++ src/test/java/taskCheckConfig/GetText.java
@@ -1,2 +1,15 @@
-package taskCheckConfig;public class getText {
+package taskCheckConfig;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetText {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://pl.wikipedia.org/");
+        WebElement element = driver.findElement(By.id("main-page-column1"));
+        System.out.println(element.getText());
+    }
 }
