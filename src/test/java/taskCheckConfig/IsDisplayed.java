ab9532679282eb91a20e983cdd2cf5966bd85082
robertdobrowolski001 <robertdobrowolski001@gmail.com>
2023-06-26 18:39:04 +0200

    Class IsDisplayed

--- src/test/java/taskCheckConfig/IsDisplayed.java
+++ src/test/java/taskCheckConfig/IsDisplayed.java
@@ -5,17 +5,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class IsEnabled {
+public class IsDisplayed {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://pl.wikipedia.org/");
         Thread.sleep(2000); //wrzucamy sleep, aby obejsc wyjatek zwiazany ze zmianami na stronie po wywolaniu
         WebElement search = driver.findElement(By.name("search"));
-        if(search.isEnabled()) {
+        if(search.isDisplayed()) {
             search.sendKeys("Selenium");
             search.submit();
         }else {
-            System.out.println("Element is disabled");
+            System.out.println("Element is not visible");
         }
     }
 }
