88ac9b05387ee2319c4ca5c222dae1904bb86288
robertdobrowolski001 <robertdobrowolski001@gmail.com>
2023-06-26 18:36:21 +0200

    Class GetAttribute

--- src/test/java/taskCheckConfig/GetAttribute.java
+++ src/test/java/taskCheckConfig/GetAttribute.java
@@ -1,2 +1,14 @@
-package taskCheckConfig;public class getAttribute {
+package taskCheckConfig;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetAttribute {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://pl.wikipedia.org/");
+        String atrybut = driver.findElement(By.id("main-page-content")).getAttribute("class");
+        System.out.println(atrybut);
+    }
 }
