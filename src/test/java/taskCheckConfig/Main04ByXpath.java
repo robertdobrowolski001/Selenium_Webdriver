02759c4b4a204068480911d8e39d2ca130057040
robertdobrowolski001 <robertdobrowolski001@gmail.com>
2023-06-26 18:44:48 +0200

    Class Main04ByXpath

--- src/test/java/taskCheckConfig/Main04ByXpath.java
+++ src/test/java/taskCheckConfig/Main04ByXpath.java
@@ -1,23 +1,34 @@
-package LocatingElements;
+package taskCheckConfig;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class ByXpath {
-    public static void main(String[] args) {
-
+public class Main04ByXpath {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://hotel-testlab.coderslab.pl/en/");
+        driver.findElement(By.className("user_login")).click();
 
-        WebElement signInButton = driver.findElement(By.className("hide_xs"));
-        WebElement enterEmail = driver.findElement(By.className("account_input"));
+//        WebElement signInButton = driver.findElement(By.className("user_login"));
+//        signInButton.click();
 
-        signInButton.click();
-        enterEmail.sendKeys("bo0@wp.pl");
+        driver.findElement(By.className("account_input")).sendKeys("b9o0o84@wp.pl");
 
-        WebElement createAccountButton = driver.findElement(By.id("SubmitCreate"));
-        createAccountButton.click();
+//        WebElement enterEmail = driver.findElement(By.className("account_input"));
+//        enterEmail.sendKeys("b9o0o84@wp.pl");
+
+        driver.findElement(By.id("SubmitCreate")).click();
+
+//        WebElement createAccountButton = driver.findElement(By.id("SubmitCreate"));
+//        createAccountButton.click();
+
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@id='customer_firstname']")).sendKeys("John");
+        driver.findElement(By.xpath("//input[@name='customer_lastname']")).sendKeys("Doe");
+        driver.findElement(By.xpath("//input[@data-validate='isPasswd']")).sendKeys("secretpass");
+        driver.findElement(By.xpath("//button[@id='submitAccount']")).click();
     }
 }
