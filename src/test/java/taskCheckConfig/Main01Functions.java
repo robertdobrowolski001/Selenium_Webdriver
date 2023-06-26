48a686186d3155c13c894eb8efa0ded011da642b
robertdobrowolski001 <robertdobrowolski001@gmail.com>
2023-06-26 18:41:28 +0200

    Class Main01Functions

--- src/test/java/taskCheckConfig/Main01Functions.java
+++ src/test/java/taskCheckConfig/Main01Functions.java
@@ -5,30 +5,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Main04ByXpath {
+public class Main01Functions {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://hotel-testlab.coderslab.pl/en/");
         driver.findElement(By.className("user_login")).click();
-
-//        WebElement signInButton = driver.findElement(By.className("user_login"));
-//        signInButton.click();
-
-        driver.findElement(By.className("account_input")).sendKeys("b9o0o84@wp.pl");
-
-//        WebElement enterEmail = driver.findElement(By.className("account_input"));
-//        enterEmail.sendKeys("b9o0o84@wp.pl");
-
+        driver.findElement(By.className("account_input")).sendKeys("b9o0o884@wp.pl");
         driver.findElement(By.id("SubmitCreate")).click();
-
-//        WebElement createAccountButton = driver.findElement(By.id("SubmitCreate"));
-//        createAccountButton.click();
-
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//input[@id='customer_firstname']")).sendKeys("John");
-        driver.findElement(By.xpath("//input[@name='customer_lastname']")).sendKeys("Doe");
-        driver.findElement(By.xpath("//input[@data-validate='isPasswd']")).sendKeys("secretpass");
-        driver.findElement(By.xpath("//button[@id='submitAccount']")).click();
+        WebElement firstNameInput = driver.findElement(By.xpath("//input[@id='customer_firstname']"));
+        if(firstNameInput.isDisplayed()) {
+            firstNameInput.sendKeys("John");
+        }
+        WebElement lastNameInput = driver.findElement(By.xpath("//input[@name='customer_lastname']"));
+        if(lastNameInput.isDisplayed()) {
+            lastNameInput.sendKeys("Doe");
+        }
+        WebElement passwdInput = driver.findElement(By.xpath("//input[@data-validate='isPasswd']"));
+        if(passwdInput.isDisplayed()) {
+            passwdInput.sendKeys("secretpass");
+        }
+        WebElement submitBtn = driver.findElement(By.xpath("//button[@id='submitAccount']"));
+        if (submitBtn.isDisplayed() && submitBtn.isEnabled()) {
+            submitBtn.click();
+    }
     }
 }
