0de4b4268d9eb40dab964d12ecc64077673c90dc
robertdobrowolski001 <robertdobrowolski001@gmail.com>
2023-06-26 18:43:12 +0200

    Class Main02ByName

--- src/test/java/taskCheckConfig/Main02ByName.java
+++ src/test/java/taskCheckConfig/Main02ByName.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class ByName {
+public class Main02ByName {
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
