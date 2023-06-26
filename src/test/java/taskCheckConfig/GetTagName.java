4c0531059e97f11c5e6be34c6ebd868273315a6c
robertdobrowolski001 <robertdobrowolski001@gmail.com>
2023-06-26 18:37:05 +0200

    Class GetTagName

--- src/test/java/taskCheckConfig/GetTagName.java
+++ src/test/java/taskCheckConfig/GetTagName.java
@@ -4,11 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GetAttribute {
+public class GetTagName {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://pl.wikipedia.org/");
-        String atrybut = driver.findElement(By.id("main-page-content")).getAttribute("class");
-        System.out.println(atrybut);
+        String tag = driver.findElement(By.id("main-page-content")).getTagName();
+        System.out.println(tag);
     }
 }
