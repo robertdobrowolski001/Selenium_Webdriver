a12f02014e5081d3a275d89643efe2e9d2ba2fcc
robertdobrowolski001 <robertdobrowolski001@gmail.com>
2023-06-26 18:35:07 +0200

    Class AmberSearch

--- src/test/java/taskCheckConfig/AmberSearch.java
+++ src/test/java/taskCheckConfig/AmberSearch.java
@@ -1,2 +1,23 @@
-package taskCheckConfig;public class AmberSearch {
+package taskCheckConfig;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class AmberSearch {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver(); //tworzymy obiekt drivera - wybieramy przeglądarkę Chrome
+        driver.manage().window().maximize(); //Maksymalizacja okna przeglądarki
+        driver.get("http://www.google.com"); //wpisujemy adres przeglądarki
+        WebElement acceptCookiesButton = driver.findElement(By.id("L2AGLb")); //wyszukanie elementu "Zaakceptuj wszystkie
+        acceptCookiesButton.click(); //klikamy w przycisk "zaakceptuj wszystko"
+        WebElement element = driver.findElement(By.name("q")); //wyszukujemy element searchbar na stronie
+        element.clear(); // usuwamy napisy z pola searchbar
+        element.sendKeys("Roger Zelazny Amber"); //przesyłamy ciąg znaków Coderslab do searchbara
+        Thread.sleep(5000);
+        element.submit(); // akceptujemy formularz, wyszukujemy hasło w Google - równoznaczne w wciśnięciem enter
+        driver.quit();
+    }
 }
